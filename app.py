4094a65c419de5f6b92423121e9301f6ea418acf
Arya Bharne <127336304+arya-io@users.noreply.github.com>
2024-11-07 21:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ from utils import clean_text
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("📧 Cold Mail Generator")
 
+    st.markdown("""
+    This app is a **Cold Email Generator** designed to help business professionals quickly generate personalized cold emails for job opportunities 
+    listed on websites. It allows users to input a URL for a job listing, then extracts relevant job details (such as role, skills, and experience) 
+    from the page using AI. The user can also input their name, designation, and company details, which are then used to automatically craft a professional 
+    cold email tailored to the job description. The app also integrates the user’s company portfolio links to showcase relevant work.
+    """)
+
     # User Inputs for name, designation, and company
-    user_name = st.text_input("Enter your name:", placeholder="Mohan")
-    user_designation = st.text_input("Enter your designation:", placeholder="BDE")
-    user_company = st.text_input("Enter your company name:", placeholder="AtliQ")
+    user_name = st.text_input("Enter your name:", placeholder="Your Name")
+    user_designation = st.text_input("Enter your designation:", placeholder="Your Designation in the Company")
+    user_company = st.text_input("Enter your company name:", placeholder="Your Company Name")
     
     url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-43835?from=job%20search%20funnel")
     submit_button = st.button("Submit")
